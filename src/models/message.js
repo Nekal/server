23692c54fbfbc1c202a1db44508d36771a6644f1
Nekal <infair69@gmail.com>
2018-02-15 20:01:35 +0300

    fix

--- src/models/message.js
+++ src/models/message.js
@@ -6,24 +6,6 @@ module.exports = (sequelize, Sequelize) => {
       userId: { type: Sequelize.INTEGER, validate: { notEmpty: true }},
       recipientId: { type: Sequelize.INTEGER, validate: { notEmpty: true }},
     },
-    // {
-    //   classMethods: {
-    //     associate(models) {
-    //       console.log('safsda')
-    //       models.Message.belongsTo(models.User, {
-    //         foreignKey: 'userId',
-    //         // allowNull: false,
-    //         // onDelete: 'CASCADE'
-    //       })
-    //
-    //       models.Message.belongsTo(models.User, {
-    //         foreignKey: 'recipientId',
-    //         // allowNull: false,
-    //         // onDelete: 'CASCADE'
-    //       })
-    //     }
-    //   },
-    // }
   );
   Message.associate = function (models) {
     console.log('safsda')
