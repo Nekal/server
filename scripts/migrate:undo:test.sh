c5b3addb803073140884f05090993d84da887f97
dlapkovsky <Welcome12>
2018-10-08 10:00:23 +0300

    delete node_modules

--- scripts/migrate:undo:test.sh
+++
@@ -1,2 +0,0 @@
-sequelize db:migrate:undo:all --to 20180124114632-create-table-news.js --env=test
-sequelize db:migrate:undo:all --to 20180124114619-create-table-users.js --env=test
--- scripts/seeds:undo:test.sh
+++
@@ -1,3 +0,0 @@
-sequelize db:seed:undo --seed 20180208080639-admin.js --env=test
-sequelize db:seed:undo --seed 20180208080958-test-news.js --env=test
-sequelize db:seed:undo --seed 20180208081323-test-news.js --env=test
